b42f3bd6b5be2a037a3f6dd2f013f52483e18dd0
codingisreallyhard <florinconstantin99@yahoo.com>
2022-08-23 17:40:44 +0300

    finished code

---
+++ src/components/UI/Store/cart-slice.js
@@ -0,0 +1,74 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          title: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending",
+        message: "Sending data",
+      })
+    );
+
+    const response = await fetch(
+      "https://react-http-ba9ff-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("sending failed");
+    }
+    dispatch(
+      uiActions.showNotification({
+        status: "success",
+        title: "Success",
+        message: "Sending cart data successfully",
+      })
+    );
+  };
+};
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
